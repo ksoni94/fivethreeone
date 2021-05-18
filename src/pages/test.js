476fe73fe27ed4a38a8e5183baa7e5c3d07dc734
Karan <karan@farewill.com>
2021-05-18 09:06:51 +0100

    add test page

---
+++ src/pages/test.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DatabaseTest = () => {
+  return (
+    <div>
+      <input />
+      <button>Send</button>
+    </div>
+  );
+};
+
+export default DatabaseTest;
